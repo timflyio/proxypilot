5b7dfab37ea0a654b9fc2f895860159ffe5d5ca5
Tim Newsham <tim@fly.io>
2025-07-09 17:07:14 -1000

    fixup python3's CA. ugh...

--- startup.sh
+++ startup.sh
@@ -2,6 +2,8 @@
 
 echo "adding our CA and redirecting api.github.com"
 cat /ca.pem >> /etc/ssl/certs/ca-certificates.crt
+cat /ca.pem >> `python3 -c "import certifi; print(certifi.where())"`
+
 echo "::1 api.github.com api.openai.com api.anthropic.com" >> /etc/hosts
 
 echo "spinning waiting for shells"
