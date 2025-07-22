4935511b4f0b59d711012ee032f76e5af9b077d1
Tim Newsham <tim@fly.io>
2025-07-21 18:36:31 -1000

    use 127.0.0.1 because pilot tcp healthcheck only checks 127.0.0.1

--- startup.sh
+++ startup.sh
@@ -9,7 +9,7 @@ fi
 cat /ca.pem >> /etc/ssl/certs/ca-certificates.crt
 cat /ca.pem >> `python3 -c "import certifi; print(certifi.where())"`
 
-echo "::1 api.github.com api.openai.com api.anthropic.com" >> /etc/hosts
+echo "127.0.0.1 api.github.com api.openai.com api.anthropic.com" >> /etc/hosts
 
 echo "spinning waiting for shells"
 /bin/sleep inf
